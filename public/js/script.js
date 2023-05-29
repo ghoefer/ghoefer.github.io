27cc7581215577b6082a7c13f0d33cea386e9458
ghoefer <ghoefer@samford.edu>
2023-05-28 21:23:37 -0400

    remove complicated nav

---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+console.log("script.js loaded");
+
+document.addEventListener('DOMContentLoaded', function() {
+
+// THE FOLLOWING CODE APPENDS BUTTONS TO TOP NAVBAR ON CLICK
+    // // get all bottom column buttons
+// const bottomcolumns = document.querySelectorAll(".bottom-column");
+// // const emptycol = document.querySelectorAll(".bottom-column empty");
+// console.log(bottomcolumns);
+
+// // get top navbar
+// const topnav = document.querySelector(".topnav-parent");
+
+// // append bottom column buttons to navbar on click
+// bottomcolumns.forEach((bottomcolumn) => {
+//     console.log("made it here!");
+//     bottomcolumn.addEventListener("click", () => {
+//         console.log("clicked!");
+//         topnav.appendChild(bottomcolumn);
+//         // navbar.appendChild(emptycol);
+
+//     });
+// });
+
+});
+
+
+
+// Path: public/js/script.js
