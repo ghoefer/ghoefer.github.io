fd489fa59751c0e489a62477cf317a4101e0d2de
ghoefer <ghoefer@samford.edu>
2023-10-03 12:06:50 -0400

    working navbar js

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
   /* Navbar auto hide/show feature is bootstrapped
   Find it here https://bootstrap-menu.com/detail-autohide.html */
-  el_autohide = document.querySelector('.autohide');
+  // el_autohide = document.querySelector('.autohide');
 
-  console.log("is this working");
-  // add padding-top to body (if necessary)
-      navbar_height = document.querySelector('#nav-placeholder').offsetHeight;
-      document.body.style.paddingTop = navbar_height + 'px';
+  // console.log("is this working");
+  // // add padding-top to body (if necessary)
+  //     navbar_height = document.querySelector('#nav-placeholder').offsetHeight;
+  //     document.body.style.paddingTop = navbar_height + 'px';
 
-  if(el_autohide){
-  var last_scroll_top = 0;
-  window.addEventListener('scroll', function() {
-          let scroll_top = window.scrollY;
-      if(scroll_top < last_scroll_top) {
-              el_autohide.classList.remove('scrolled-down');
-              el_autohide.classList.add('scrolled-up');
-          }
-          else {
-              el_autohide.classList.remove('scrolled-up');
-              el_autohide.classList.add('scrolled-down');
-          }
-          last_scroll_top = scroll_top;
-  }); 
-  // window.addEventListener
-  }
-  // if
+  // if(el_autohide){
+  // var last_scroll_top = 0;
+  // window.addEventListener('scroll', function() {
+  //         let scroll_top = window.scrollY;
+  //     if(scroll_top < last_scroll_top) {
+  //             el_autohide.classList.remove('scrolled-down');
+  //             el_autohide.classList.add('scrolled-up');
+  //         }
+  //         else {
+  //             el_autohide.classList.remove('scrolled-up');
+  //             el_autohide.classList.add('scrolled-down');
+  //         }
+  //         last_scroll_top = scroll_top;
+  // }); 
+  // // window.addEventListener
+  // }
+  // // if
 }); 
 // DOMContentLoaded  end hi
