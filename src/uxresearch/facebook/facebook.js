4b5423d8cc98ac8bb4728196d439ef745fb433dd
ghoefer <ghoefer@samford.edu>
2024-01-26 18:18:59 -0500

    updated coming soon to reflect youtube shorts

--- src/uxresearch/facebook/facebook.js
+++ src/uxresearch/facebook/facebook.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {    var videoContainers = document.querySelectorAll('.video-container');
+document.addEventListener("DOMContentLoaded", function() {    
+    var videoContainers = document.querySelectorAll('.video-container');
+    
     videoContainers.forEach(container => {
         var playButton = container.querySelector('.play-button');        
         playButton.addEventListener('click', function() {
             var videoId = container.getAttribute('data-video-id');            
             var iframe = document.createElement('iframe');
-            iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=0&mute=1`);            
+            iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=0&mute=1&shorts=1`);            
             iframe.setAttribute('frameborder', '0');
             iframe.setAttribute('allowfullscreen', '1');            
             iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+            iframe.setAttribute('width','297');
+            iframe.setAttribute('height', '528');
+            iframe.setAttribute('align-self', 'center');
             container.innerHTML = '';
             container.appendChild(iframe);        
         });
-    });});
+    });
+});
