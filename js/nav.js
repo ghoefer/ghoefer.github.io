c28111a7110b92211e086285056066c7238a2472
ghoefer <ghoefer@samford.edu>
2024-02-02 21:38:55 -0500

    navbar js test

--- js/nav.js
+++ js/nav.js
@@ -9,30 +9,15 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    el_autohide = document.querySelector('.autohide');
-
-    // add padding-top to bady (if necessary)
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-    document.body.style.paddingTop = navbar_height + 'px';
-
-    if (el_autohide) {
-
-      var last_scroll_top = 0;
-      window.addEventListener('scroll', function () {
-        let scroll_top = window.scrollY;
-        if (scroll_top < last_scroll_top) {
-          el_autohide.classList.remove('scrolled-down');
-          el_autohide.classList.add('scrolled-up');
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
         } else {
-          el_autohide.classList.remove('scrolled-up');
-          el_autohide.classList.add('scrolled-down');
+            document.getElementById("navbar").style.top = "-50px";
         }
-        last_scroll_top = scroll_top;
-
-      });
-      // window.addEventListener
-
-    }
-    // if
+        prevScrollpos = currentScrollPos;
+        }
 
   });
