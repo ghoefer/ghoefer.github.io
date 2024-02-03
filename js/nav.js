501801da3c023517fbfe0250256ae0b0aaebb37d
ghoefer <ghoefer@samford.edu>
2024-02-02 21:41:52 -0500

    navbar js test

--- js/nav.js
+++ js/nav.js
@@ -5,19 +5,17 @@
 
 
   /* Navbar auto hide/show feature is bootstrapped
-  Find it here https://bootstrap-menu.com/detail-autohide.html */
+  Find it here https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
 
-document.addEventListener("DOMContentLoaded", function () {
 
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navbar").style.top = "0";
-        } else {
-            document.getElementById("navbar").style.top = "-50px";
-        }
-        prevScrollpos = currentScrollPos;
-        }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+    }
 
-  });
