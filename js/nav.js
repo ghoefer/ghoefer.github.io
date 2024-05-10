c496ba6eb82d403b0398ddf00fb84161acafa552
ghoefer <ghoefer@samford.edu>
2024-05-10 17:28:18 -0400

    navbar fix

--- js/nav.js
+++ js/nav.js
@@ -8,28 +8,28 @@ function navfunction() {
     Find it here https://bootstrap-menu.com/detail-autohide.html */
   
   
-    el_autohide = document.querySelector('.autohide');
-    el_autohide.style.position = "fixed";
+//     el_autohide = document.querySelector('.autohide');
+//     el_autohide.style.position = "fixed";
   
-  //   console.log("is this working");
-    // add padding-top to body (if necessary)
-        navbar_height = document.querySelector('.navbar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
+//   //   console.log("is this working");
+//     // add padding-top to body (if necessary)
+//         navbar_height = document.querySelector('.navbar').offsetHeight;
+//         document.body.style.paddingTop = navbar_height + 'px';
   
-    if(el_autohide){
-    var last_scroll_top = 0;
-    window.addEventListener('scroll', function() {
-            let scroll_top = window.scrollY;
-        if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
-            }
-            else {
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
-            }
-            last_scroll_top = scroll_top;
-    }); 
+//     if(el_autohide){
+//     var last_scroll_top = 0;
+//     window.addEventListener('scroll', function() {
+//             let scroll_top = window.scrollY;
+//         if(scroll_top < last_scroll_top) {
+//                 el_autohide.classList.remove('scrolled-down');
+//                 el_autohide.classList.add('scrolled-up');
+//             }
+//             else {
+//                 el_autohide.classList.remove('scrolled-up');
+//                 el_autohide.classList.add('scrolled-down');
+//             }
+//             last_scroll_top = scroll_top;
+//     }); 
     // window.addEventListener
     }
     // if
